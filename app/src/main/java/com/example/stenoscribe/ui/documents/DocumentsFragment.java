7b122c2f52bde219c145883bf824b70ca8ac13d7
Matthew <madstone@ucsc.edu>
2019-11-11 23:00:57 -0800

    required to update. My code was almost bricked due to a signature error and it cost me dearly.

--- app/src/main/java/com/example/stenoscribe/ui/documents/DocumentsFragment.java
+++ app/src/main/java/com/example/stenoscribe/ui/documents/DocumentsFragment.java
@@ -1,9 +1,15 @@
 package com.example.stenoscribe.ui.documents;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,33 +18,45 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.stenoscribe.MeetingDetails;
 import com.example.stenoscribe.R;
+import com.example.stenoscribe.db.AppDatabase;
+import com.example.stenoscribe.db.File;
+import com.example.stenoscribe.db.FileAccessor;
+import com.example.stenoscribe.db.FileOperator;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.List;
+
 public class DocumentsFragment extends Fragment {
-
+    private ListView document_viewer;
     private DocumentsViewModel documentsViewModel;
+    private AppDatabase database;
+    private FileAccessor access;
+    private FloatingActionButton fab;
+    private List<File> documents;
+    private ListView listView;
+    private FileOperator io;
+    private int meetingId;
+    private final String type = "recording";
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        documentsViewModel =
-                ViewModelProviders.of(this).get(DocumentsViewModel.class);
+    private String TAG = "DOCUMENTSFRAGMENT";
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        this.meetingId = ((MeetingDetails)getActivity()).getUid();
+        documentsViewModel = ViewModelProviders.of(this).get(DocumentsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_documents, container, false);
-//        final TextView textView = root.findViewById(R.id.text_documents);
-//        documentsViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-
+        document_viewer = (ListView) root.findViewById(R.id.document_view);
+        this.database = AppDatabase.getDatabase(root.getContext());
+        this.access = new FileAccessor(this.database);
+        documents = access.listFiles(meetingId,"document");
+        DocumentAdapter docudapt = new DocumentAdapter(this,documents);
+        document_viewer.setAdapter(docudapt);
         FloatingActionButton fab = root.findViewById(R.id.fab_documents);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "This lets you upload a new document", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // lost due to almost bricking
             }
         });
 
--- app/src/main/java/com/example/stenoscribe/ui/documents/DocumentsViewModel.java
+++ app/src/main/java/com/example/stenoscribe/ui/documents/DocumentsViewModel.java
@@ -1,18 +1,54 @@
 package com.example.stenoscribe.ui.documents;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.stenoscribe.R;
+import com.example.stenoscribe.db.File;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
 public class DocumentsViewModel extends ViewModel {
-
     private MutableLiveData<String> mText;
-
     public DocumentsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is documents fragment");
     }
-
+    private Context current_context;
+    private ArrayList<File> Document_list;
+    public class DocumentAdapter extends ArrayAdapter<File> {
+        private List<File> Documents;
+        private DocumentAdapter(Context context,int rId, ArrayList<File> Documents) {
+            super(context, rId, Documents);
+            current_context = context;
+            Document_list = Documents;
+        }
+        @Override
+        public View getView(int position, View v, ViewGroup parent) {
+            final TextView title;
+            if (v == null) {
+                LayoutInflater vi = (LayoutInflater)current_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(R.layout.fragment_document_layout, null);
+            }
+            File Document = Document_list.get(position);
+            if (Document != null) {
+                title = v.findViewById(R.id.document_title);
+                title.setText(Document.type);
+            }
+            return v;
+        }
+    }
     public LiveData<String> getText() {
         return mText;
     }
